309e257e771dc5540becb5c4d29eef5075abb0c4
tuchayouji <2389897360@qq.com>
2026-06-17 19:54:51 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "Tucha的个人博客",
 	subtitle: "记录动漫、代码与日常随笔",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://tuchayouji.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-06-17", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
