49558e602587f0d055ab400df3d8a214897984bf
tuchayouji <tuchayouji@users.noreply.github.com>
2026-06-17 21:46:06 +0800

    update

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -8,7 +8,7 @@ export const announcementConfig: AnnouncementConfig = {
 	link: {
 		enable: true, // 启用链接
 		text: "了解更多", // 链接文本
-		url: "/about/", // 链接 URL
+		url: "/me/", // 链接 URL
 		external: false, // 内部链接
 	},
 };
